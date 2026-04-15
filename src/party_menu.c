4bfb8f2008ed644a3f816c09e811ad179fa4d17f
RoamerX <20692776+RoamerX@users.noreply.github.com>
2026-04-15 16:38:17 +0800

    case语句添加大括号以通过编译

--- src/party_menu.c
+++ src/party_menu.c
@@ -1962,6 +1962,7 @@ static void UpdatePartySelectionDoubleLayout(s8 *slotPtr, s8 movementDir)
         }
         break;
     case MENU_DIR_RIGHT:
+        {
         u8 menuId;
 
         if (*slotPtr == 0)
@@ -1978,6 +1979,7 @@ static void UpdatePartySelectionDoubleLayout(s8 *slotPtr, s8 movementDir)
         if (GetMonData(&party[partySlot], MON_DATA_SPECIES) != SPECIES_NONE)
             *slotPtr = menuId;
         break;
+        }
     case MENU_DIR_LEFT:
         if (*slotPtr == 2 || *slotPtr == 3)
         {
